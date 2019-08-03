3c5eecf5803578f1fba287ad86795e19c0063968
Andrey Lutogin <lutogin@trueconf.com>
2019-08-03 11:30:39 +0300

    Add allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '=1h)0n-_)37*xx8eohe!#cb@jy+j_u)sh9-*egp(*@ic3t$%++'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'evening-taiga-33144.herokuapp.com'
+    'evening-taiga-33144.herokuapp.com',
+    '127.0.0.1'
 ]
 
 
